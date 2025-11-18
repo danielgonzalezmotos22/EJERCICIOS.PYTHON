d3ac59fc8d0a31087385fae19517d110575cc60f
danielgonzalezmotos22 <danielgonzalezmotos22@gmail.com>
2025-11-18 13:43:46 +0100

    Update ejercicios.py

--- ejercicios.py
+++ ejercicios.py
@@ -1,1 +1,13 @@
-print ("Hola putos")
+import  random
+a = random.randint (1, 10)
+def aleatorio (numero):
+    print (a)
+
+if __name__ == "__main__":
+    numero = input("Adivina el numero: ")
+
+if numero == a:
+    print("Has hacertado")
+
+else:
+    print ("Te has equivocado",f"el numero es {a}")
